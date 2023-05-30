09c3b592e0e8be898274976c7c4d644b06800ccf
Moataz Khallaf <MoatazKhallaf@outlook.com>
2023-05-30 16:39:31 -0600

    first commit

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Gallary from '~/components/Gallary'
+// import '~/App.css'
+
+export default function LandingPage() {
+    return (
+        <div>
+            <Gallary />
+        </div>
+    )
+}
